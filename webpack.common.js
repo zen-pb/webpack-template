099bd7f4ede4c62ce2b13ddffd8e45e5278c44fa
zen-pb <111935236+zen-pb@users.noreply.github.com>
2025-02-02 17:00:59 +0800

    Add babel

--- webpack.common.js
+++ webpack.common.js
@@ -27,6 +27,17 @@ module.exports = {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: "asset/resource",
       },
+      {
+        test: /\.(?:js|mjs|cjs)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            targets: "defaults",
+            presets: [["@babel/preset-env"]],
+          },
+        },
+      },
     ],
   },
 };
